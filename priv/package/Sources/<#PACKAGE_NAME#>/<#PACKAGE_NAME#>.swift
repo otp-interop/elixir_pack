de095465606bbf408dc44b7969e670db0f1da526
Carson Katri <Carson.katri@gmail.com>
2025-04-01 10:56:44 -0400

    Update Swift API with automatic port assignment and Observation support

--- priv/package/Sources/<#PACKAGE_NAME#>/<#PACKAGE_NAME#>.swift
+++ priv/package/Sources/<#PACKAGE_NAME#>/<#PACKAGE_NAME#>.swift
@@ -1,62 +1,127 @@
+import Observation
 import Foundation
 import Combine
 import erlang
 
-public struct <#PACKAGE_NAME#> {
-    static func elixirStart(
-        bundle: Bundle,
-        host: String,
-        port: Int,
-        secretKeyBase: String
+@Observable
+public final class <#PACKAGE_NAME#> {
+    let thread: Thread
+    public let port: Int
+
+    public init(
+        host: String = "127.0.0.1",
+        port: Int? = nil,
+        secretKeyBase: String = "<#SECRET_KEY_BASE#>"
     ) {
-        let rootdir = bundle.path(forResource: "_elixirkit_build", ofType: nil)!
-        let bindir = "\(rootdir)/releases/0.1.0"
-        let bootdir = "\(bindir)/start"
-        let configdir = "\(bindir)/sys"
-        let libdir = "\(rootdir)/lib"
+        let port = port ?? Self.automaticPort()
+        self.port = port
+        self.thread = Thread {
+            // Phoenix environment variables
+            setenv("SECRET_KEY_BASE", secretKeyBase, 0)
+            setenv("PHX_SERVER", "true", 0)
+            setenv("PHX_HOST", host, 0)
+            setenv("PORT", String(port), 0)
+
+            Self.start()
+        }
+        self.thread.start()
+    }
+
+    deinit {
+        thread.cancel()
+    }
+
+    public static func start(
+        with arguments: [String] = []
+    ) {
+        let rootPath = Bundle.module.path(forResource: "_elixirkit_build", ofType: nil)!
+        let binPath = "\(rootPath)/releases/0.1.0"
+        let bootPath = "\(binPath)/start"
+        let configPath = "\(binPath)/sys"
+        let libPath = "\(rootPath)/lib"
         
-        setenv("BINDIR", bindir, 0)
-        setenv("ERL_LIBS", rootdir, 0)
-        setenv("RELEASE_SYS_CONFIG", configdir, 0)
-        setenv("RELEASE_ROOT", rootdir, 0)
-        setenv("SECRET_KEY_BASE", secretKeyBase, 0)
-        setenv("PHX_SERVER", "true", 0)
-        setenv("PHX_HOST", host, 0)
-        setenv("PORT", String(port), 0)
+        setenv("BINDIR", binPath, 0)
+        setenv("ERL_LIBS", rootPath, 0)
+        setenv("RELEASE_SYS_CONFIG", configPath, 0)
+        setenv("RELEASE_ROOT", rootPath, 0)
         
-        var args: [UnsafeMutablePointer<CChar>?] = [
+        var args: [UnsafeMutablePointer<CChar>?] = ([
             "elixirkit",
             "--",
-            "-bindir", bindir,
-            "-root", rootdir,
+            "-bindir", binPath,
+            "-root", rootPath,
             "-noshell",
             "-start_epmd", "false",
-            "-boot", bootdir,
-            "-boot_var", "RELEASE_LIB", libdir,
+            "-boot", bootPath,
+            "-boot_var", "RELEASE_LIB", libPath,
             "-interactive",
-            "-pa", rootdir,
+            "-pa", rootPath,
             "-kernel", "inet_dist_use_interface", "{127,0,0,1}",
-            "-config", configdir,
-        ]
+            "-config", configPath,
+        ] + arguments)
             .map {
                 $0.withCString { strdup($0) }
             }
         
         erlang.erl_start(Int32(args.count), &args)
     }
+
+    /// Port 4000, used if ``automaticPort`` fails.
+    static var fallbackPort: Int { 4000 }
     
-    public static func start(
-        host: String = "127.0.0.1",
-        port: Int = 4000,
-        secretKeyBase: String = "<#SECRET_KEY_BASE#>"
-    ) {
-        Thread {
-            elixirStart(
-                bundle: Bundle.module,
-                host: host,
-                port: port,
-                secretKeyBase: secretKeyBase
-            )
-        }.start()
+    /// Automatic port assignment that falls back to ``fallbackPort``.
+    static func automaticPort() -> Int {
+        // create a server bound to port 0
+        let server = socket(AF_INET, SOCK_STREAM, 0)
+        
+        guard server != -1 // failed to create
+        else { return fallbackPort }
+        
+        defer { close(server) }
+        
+        var address = sockaddr_in()
+        address.sin_family = sa_family_t(AF_INET)
+        address.sin_addr.s_addr = INADDR_ANY
+        address.sin_port = 0 // auto port assignment
+        
+        var addressLength = socklen_t(MemoryLayout<sockaddr_in>.stride)
+        
+        guard withUnsafeMutablePointer(to: &address, {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                let isBound = bind(server, $0, addressLength) == 0
+                let isAddressSet = getsockname(server, $0, &addressLength) == 0
+                return isBound && isAddressSet
+            }
+        })
+        else { return fallbackPort }
+        
+        guard listen(server, 1) == 0
+        else { return fallbackPort }
+        
+        // connect to the server
+        let client = socket(AF_INET, SOCK_STREAM, 0)
+        
+        guard client != -1 // failed to create
+        else { return fallbackPort }
+        
+        defer { close(client) }
+        
+        guard withUnsafeMutablePointer(to: &address, {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                connect(client, $0, addressLength) == 0
+            }
+        })
+        else { return fallbackPort }
+        
+        // accept the connection
+        let acceptedSocket = accept(server, nil, nil)
+        
+        guard acceptedSocket != -1 // failed to accept
+        else { return fallbackPort }
+        
+        defer { close(acceptedSocket) }
+        
+        // return the port assigned to the server
+        return Int(address.sin_port.byteSwapped)
     }
 }
